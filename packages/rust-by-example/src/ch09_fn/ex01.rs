314e707d5bcac2b26a2e0c4ba795e9e2594ff3ef
hhstore <hhglory@outlook.com>
2022-02-18 23:55:00 +0800

    update: rust-by-example > ch09

--- packages/rust-by-example/src/ch09_fn/ex01.rs
+++ packages/rust-by-example/src/ch09_fn/ex01.rs
@@ -31,3 +31,96 @@ fn ex01_fn() {
 
     fizzbuzz_to(100);
 }
+
+#[test]
+fn ex01_methods() {
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    impl Point {
+        // 默认值:
+        fn origin() -> Point {
+            Point { x: 0.0, y: 0.0 }
+        }
+
+        // 构造方法:
+        fn new(x: f64, y: f64) -> Point {
+            Point { x, y }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    struct Rectangle {
+        p1: Point,
+        p2: Point,
+    }
+
+    impl Rectangle {
+        fn area(&self) -> f64 {
+            // TODO X: 解包: 得出 (x1, y1) 值, 类似 Python 元组解包
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+
+            println!("unpack: ({}, {}), ({}, {})", x1, y1, x2, y2);
+
+            // return:
+            ((x1 - x2) * (y1 - y2)).abs()
+        }
+
+        fn perimeter(&self) -> f64 {
+            // TODO X: 解包: 得出 (x1, y1) 值, 类似 Python 元组解包
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+            println!("unpack: ({}, {}), ({}, {})", x1, y1, x2, y2);
+
+            // return:
+            2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+        }
+
+        fn translate(&mut self, x: f64, y: f64) {
+            self.p1.x += x;
+            self.p2.x += x;
+
+            self.p1.y += y;
+            self.p2.y += y;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // TODO X: `Pair` 拥有资源：两个堆分配的整型
+    struct Pair(Box<i32>, Box<i32>);
+
+    impl Pair {
+        fn destroy(self) {
+            // TODO X: 解包: 类似 Python 元组解包
+            let Pair(first, second) = self;
+
+            println!("Destroying Pair({}, {})", first, second);
+            // TODO X: `first` 和 `second` 离开作用域后释放
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+
+    let rectangle = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+    pair.destroy();
+    // pair.destroy(); // 不能再调用
+}
--- packages/rust-by-example/src/ch09_fn/ex02.rs
+++ packages/rust-by-example/src/ch09_fn/ex02.rs
@@ -1,4 +1,86 @@
+use std::mem;
+
 #[test]
-fn ex02_xx() {
-    //
+fn ex02_closures() {
+    // 普通自增加法:
+    fn function(i: i32) -> i32 {
+        i + 1
+    }
+
+    // TODO X: 闭包写法1:
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    // TODO X: 闭包写法2: 简写
+    let closure_inferred = |i| i + 1;
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    let i = 1;
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // TODO X: 无参闭包,自动推导返回值类型
+    let one = || 1;
+    println!("closure returning one: {}", one());
+}
+
+// 捕获:
+#[test]
+fn ch02_01_capture() {
+    let color = String::from("green");
+
+    let print = || println!("`color`: {}", color);
+    print();
+
+    // 只读引用:
+    let _reborrow = &color;
+    print();
+
+    // 赋值:
+    let _color_moved = color;
+    // print();
+
+    let mut count = 0;
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    inc();
+
+    // let _reborrow = &count;
+    inc();
+
+    let _count_reborrowed = &mut count;
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // 被闭包捕获:
+    let movable = Box::new(3);
+
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+
+        // TODO X: 闭包, 捕获外部变量
+        mem::drop(movable);
+    };
+
+    consume();
+    // TODO X: error
+    // consume();
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    let haystack = vec![1, 2, 3];
+
+    // TODO X: move + 闭包, 强制获取所有权
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("contains: {}", contains(&1));
+    println!("contains: {}", contains(&4));
+
+    // error: value moved
+    // println!("`haystack`: {}", haystack.len());
 }
