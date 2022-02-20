2e89053bdf540e1715f634336d6cd5b8a906c61a
hhstore <hhglory@outlook.com>
2022-02-20 15:58:42 +0800

    update: rust-by-example > ch09

--- packages/rust-by-example/src/ch09_fn/ex03.rs
+++ packages/rust-by-example/src/ch09_fn/ex03.rs
@@ -1,4 +1,31 @@
 #[test]
-fn ex03_xx() {
-    //
+fn ex03_hof() {
+    fn is_odd(n: u32) -> bool {
+        n % 2 == 1
+    }
+
+    println!("find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+    let mut acc = 0;
+
+    //  写法1:
+    for n in 0.. {
+        let n_squared = n * n;
+        if n_squared >= upper {
+            break;
+        } else if is_odd(n_squared) {
+            acc += n_squared;
+        }
+    }
+
+    println!("imperative style: {}", acc);
+
+    // TODO X: 写法2: 函数式写法
+    let sum_of_squared_odd_numbers: u32 = (0..)
+        .map(|n| n * n)
+        .take_while(|&n| n < upper)
+        .filter(|&n| is_odd(n))
+        .fold(0, |sum, i| sum + i);
+
+    println!("functional style: {}", sum_of_squared_odd_numbers);
 }
--- packages/rust-by-example/src/ch09_fn/ex04.rs
+++ packages/rust-by-example/src/ch09_fn/ex04.rs
@@ -1,4 +1,44 @@
 #[test]
-fn ex04_xx() {
-    //
+#[allow(dead_code)]
+fn ex04_diverging() {
+    fn foo() -> ! {
+        panic!("This function never returns!");
+    }
+
+    // return
+    fn some_fn() {
+        ()
+    }
+
+    let a = some_fn();
+
+    println!("this function returns and you can see this line: {:?}", a);
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // let x = panic!("This function never returns!");
+    // println!("You will never see this line!");
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+
+        for i in 0..up_to {
+            let addition: u32 = match i % 2 == 1 {
+                true => i,
+                false => continue,
+            };
+
+            // sum:
+            acc += addition;
+        }
+
+        return acc;
+    }
+
+    println!(
+        "Sum of odd numbers up to 9 (excluding): {}",
+        sum_odd_numbers(9)
+    );
 }
