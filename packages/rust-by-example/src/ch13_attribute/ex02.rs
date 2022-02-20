e951271585c55f067aac558d6f3dee2d149ddb00
hhstore <hhglory@outlook.com>
2022-02-20 16:13:51 +0800

    update: rust-by-example > ch13

--- packages/rust-by-example/src/ch13_attribute/ex02.rs
+++ packages/rust-by-example/src/ch13_attribute/ex02.rs
@@ -1,4 +1,22 @@
+// #![crate_type = "lib"]
+// #![crate_name = "ex02"]
 #[test]
-fn ex02_xx() {
+fn ex02_crate() {
     //
+    pub fn public_function() {
+        println!("called `public_function()`");
+    }
+
+    fn private_function() {
+        println!("called `private_function()`");
+    }
+
+    pub fn indirect_access() {
+        println!("Called `indirect_access()`, that\n> ");
+        private_function()
+    }
+
+    //
+    public_function();
+    indirect_access();
 }
