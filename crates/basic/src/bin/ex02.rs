e804ea49deb5be8fce01c0e4edfa36f79b335b2d
hhstore <hhglory@outlook.com>
2022-03-21 19:37:10 +0800

    update: rust std: Arc + Mutex + Condvar

--- crates/basic/src/bin/ex02.rs
+++ crates/basic/src/bin/ex02.rs
@@ -4,6 +4,17 @@ use std::time::Duration;
 
 #[allow(unused)]
 fn main() {
+    /*
+    todo x: 并发代码 - 同步阻塞
+        1. 实现等待子线程先 run, 然后 main 线程接收信号, 再继续执行.
+        2. 特别说明:
+            - Arc / Mutex / Condvar 是并发安全的, 经常组合使用.(实现外部不可变, 内部可变)
+            - Arc 是外部不可变的, 需要搭配 Mutex/Condvar.
+            - Mutex 是内部可变的,
+        3. 注意作用域, 生存周期.
+
+    */
+
     let pair = Arc::new((Mutex::new(false), Condvar::new()));
     let pair2 = Arc::clone(&pair);
 
