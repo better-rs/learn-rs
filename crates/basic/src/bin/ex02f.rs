ad284877aa76a92b2c536a3cf74e3c0b26feb2ec
hhstore <hhglory@outlook.com>
2022-03-25 11:39:11 +0800

    update: rust std: sync::mpsc::sync_channel()

--- crates/basic/src/bin/ex02f.rs
+++ crates/basic/src/bin/ex02f.rs
@@ -9,7 +9,10 @@ fn main() {
     let t = thread::spawn(move || {
         let (lock, cvar) = &*pair2;
 
-        //let mut started = lock.lock().unwrap();
+        /// todo x: 官方标准用法的巧妙点:
+        ///     1. 这里只是指针方式, 改变 lock 内部元素的值, 因为没有构造新变量. 所以不会引入新的生命周期.
+        ///     2. 这种写法, 就不会因引入新变量(锁赋值), 导致 lock 阻塞整块作用域.
+        ///
         *lock.lock().unwrap() = true;
         println!("\tSub >>I'm a happy worker!");
 
