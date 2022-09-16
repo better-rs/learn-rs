5aeaff1358e64e150ac1c0064d532da798a2663d
Henry.Huang <hhglory@outlook.com>
2022-09-17 02:17:24 +0800

    update

---
+++ crates/rs-tonic/readme.md
@@ -0,0 +1,7 @@
+# Tonic 学习 & 使用:
+
+- https://github.com/hyperium/tonic
+
+> 示例:
+
+- https://github.com/hyperium/tonic/blob/master/examples/README.md
---
+++ crates/rs-tonic/src/helloworld/client.rs
@@ -0,0 +1,18 @@
+use hello_world::{greeter_client::GreeterClient, HelloRequest};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest { name: "Tonic".into() });
+
+    let response = client.say_hello(request).await?;
+
+    println!("🚀 tonic client: rpc response = {:?}", response);
+
+    Ok(())
+}
