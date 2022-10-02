3393ce6b987cc2e41006ae71f59ca5f2b9548533
Henry.Huang <hhglory@outlook.com>
2022-10-02 11:49:30 +0800

    update: sea-orm usage

--- crates/rs-try/bin/orm.rs
+++ crates/rs-try/bin/orm.rs
@@ -1,3 +1,4 @@
+use log::{info, warn};
 use sea_orm::{ConnectOptions, Database};
 
 use rs_try::orm::{mutation::Mutation, query::Query};
@@ -6,8 +7,9 @@ use std::{env, error::Error, time::Duration};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let db = Database::connect(&env::var("DATABASE_URL")?).await.unwrap();
+    pretty_env_logger::init();
 
+    // let db = Database::connect(&env::var("DATABASE_URL")?).await.unwrap();
     let db_url = &env::var("DATABASE_URL")?;
 
     // todo x: 配置 db 连接参数
@@ -17,18 +19,17 @@ async fn main() -> anyhow::Result<()> {
         .connect_timeout(Duration::from_secs(8))
         .idle_timeout(Duration::from_secs(8))
         .max_lifetime(Duration::from_secs(8))
-        .sqlx_logging(true);
+        .sqlx_logging(false);
 
     // todo x: 自带连接池 https://www.sea-ql.org/SeaORM/docs/install-and-config/connection/
     let db = Database::connect(opt).await?;
 
-    println!("db url:{:?}, conn {:?}\n", db_url, db);
+    info!("db url:{:?}, conn {:?}\n", db_url, db);
 
-    println!("===== =====\n");
+    // todo x: 查询
+    let ret = Query::get_todo(&db, 1).await?;
 
-    Query::find_post_by_id(&db, 1).await.expect("Cannot find posts in page");
-
-    println!("===== =====\n");
+    info!("todo get: {:?}", ret);
 
     Ok(())
 }
--- crates/rs-try/src/orm/query.rs
+++ crates/rs-try/src/orm/query.rs
@@ -1,12 +1,13 @@
+use log::{debug, error, info, warn};
 use rs_entity::{todos, todos::Entity as Todos};
-use sea_orm::{DbConn, DbErr, *};
+use sea_orm::{DatabaseBackend, DbConn, DbErr, *};
+
 pub struct Query;
 
 impl Query {
-    pub async fn find_post_by_id(db: &DbConn, id: i32) -> Result<Option<todos::Model>, DbErr> {
-        // Todos::find_by_id(id).one(db).await
-
-        Ok(None)
+    pub async fn get_todo(db: &DbConn, id: i32) -> Result<Option<todos::Model>, DbErr> {
+        // todo x: clion 无法自动补全
+        Todos::find_by_id(id).one(db).await
     }
 
     // If ok, returns (post models, num pages).
