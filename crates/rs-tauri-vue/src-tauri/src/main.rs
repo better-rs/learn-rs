bb051d4577ba0565d6ed4a64d33122a80ea708fa
Henry.Huang <hhglory@outlook.com>
2022-10-10 14:34:51 +0800

    update: add tauri+vue

--- crates/rs-tauri-vue/src-tauri/src/main.rs
+++ crates/rs-tauri-vue/src-tauri/src/main.rs
@@ -1,10 +1,9 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
-use tauri::api::shell;
-use tauri::{CustomMenuItem, Manager, Menu, Submenu};
+use tauri::{
+    api::shell, CustomMenuItem, Manager, Menu, Submenu, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, SystemTrayMenuItem,
+};
 
 #[tauri::command]
 fn backend_add(number: i32) -> i32 {
@@ -15,27 +14,58 @@ fn backend_add(number: i32) -> i32 {
 }
 
 fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item
+    // label.
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     let ctx = tauri::generate_context!();
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { position: _, size: _, .. } => {
+                println!("system tray received a left click");
+            },
+            SystemTrayEvent::RightClick { position: _, size: _, .. } => {
+                println!("system tray received a right click");
+            },
+            SystemTrayEvent::DoubleClick { position: _, size: _, .. } => {
+                println!("system tray received a double click");
+            },
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                },
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                },
+                _ => {},
+            },
+            _ => {},
+        })
         .invoke_handler(tauri::generate_handler![backend_add])
-        .menu(
-            tauri::Menu::os_default("Tauri Vue Template").add_submenu(Submenu::new(
-                "Help",
-                Menu::with_items([CustomMenuItem::new(
-                    "Online Documentation",
-                    "Online Documentation",
-                )
-                .into()]),
-            )),
-        )
+        .menu(tauri::Menu::os_default("Tauri Vue Template").add_submenu(Submenu::new(
+            "Help",
+            Menu::with_items([
+                CustomMenuItem::new("Online Documentation", "Online Documentation").into(),
+            ]),
+        )))
         .on_menu_event(|event| {
             let event_name = event.menu_item_id();
             match event_name {
                 "Online Documentation" => {
                     let url = "https://github.com/Uninen/tauri-vue-template".to_string();
                     shell::open(&event.window().shell_scope(), url, None).unwrap();
-                }
-                _ => {}
+                },
+                _ => {},
             }
         })
         .setup(|_app| {
