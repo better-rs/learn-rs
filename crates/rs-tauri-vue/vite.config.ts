744961a4028c3f8a0be2da4808830f8dff46269f
Henry.Huang <hhglory@outlook.com>
2022-10-10 13:15:14 +0800

    update: add tauri+vue

---
+++ crates/rs-tauri-vue/vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types="vitest" />
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+        filepath: resolve(__dirname, '.eslintrc-auto-import.json'),
+      },
+    }),
+  ],
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_'],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: './dist',
+    // See https://tauri.app/v1/references/webview-versions for details
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !!!process.env.TAURI_DEBUG,
+    sourcemap: !!process.env.TAURI_DEBUG,
+    emptyOutDir: true,
+  },
+  test: {
+    include: ['tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+})
