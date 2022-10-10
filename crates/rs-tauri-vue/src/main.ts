0ced8ad08c7126f98b09232f43499f496a76394a
Henry.Huang <hhglory@outlook.com>
2022-10-10 13:44:40 +0800

    update: add tauri+vue

--- crates/rs-tauri-vue/src/main.ts
+++ crates/rs-tauri-vue/src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 import './assets/main.postcss'
 
 if (process.env.NODE_ENV === 'development') {
-  devtools.connect('http://localhost', 8098)
+  devtools.connect('http://localhost', 5001) // TODO X: 注意和vue devtool 调试工具的端口保持一致
 }
 
 createApp(App).mount('#app')
