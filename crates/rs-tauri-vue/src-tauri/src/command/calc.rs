b15e8f4934b652f4ff972590f4bd56ed9ace2a75
Henry.Huang <hhglory@outlook.com>
2022-10-30 04:29:12 +0800

    update

--- crates/rs-tauri-vue/src-tauri/src/command/calc.rs
+++ crates/rs-tauri-vue/src-tauri/src/command/calc.rs
@@ -12,6 +12,6 @@ use window_shadows::set_shadow;
 pub fn backend_add(number: i32) -> i32 {
     // Note: these commands block the main thread and hang the UI until they return.
     // If you need to run a long-running task, use async command instead.
-    println!("Backend was called with an argument: {}", number);
+    info!("Backend was called with an argument: {}", number);
     number + 2
 }
