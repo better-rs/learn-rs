fe2904eac0df43d8cbe3e1329faba6b5c059ee7e
Henry.Huang <hhglory@outlook.com>
2022-11-03 07:54:04 +0800

    update

--- crates/rs-tauri-vue/src-tauri/src/util/app.rs
+++ crates/rs-tauri-vue/src-tauri/src/util/app.rs
@@ -1,4 +1,17 @@
-use tauri::{api::process::restart, Env, Manager, Wry};
+use tauri::{api::process::restart, AppHandle, Env, Manager, Runtime, Wry};
+
+// 重启 app:
 pub fn restart_app(env: &Env) {
     restart(env);
+}
+
+// 显示 app 主窗口:
+pub fn show_main_window(app: &AppHandle<Wry>) {
+    let win = app.get_window("main").unwrap();
+    if win.is_visible().unwrap() {
+        win.hide().unwrap();
+    } else {
+        // win.hide().unwrap();
+        win.set_focus().unwrap();
+    }
 }
