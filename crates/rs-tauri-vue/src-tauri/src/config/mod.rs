d0cdb14f14d8ef200f49baab92be1c2b23fc73e2
Henry.Huang <hhglory@outlook.com>
2022-11-03 08:04:22 +0800

    update

--- crates/rs-tauri-vue/src-tauri/src/config/mod.rs
+++ crates/rs-tauri-vue/src-tauri/src/config/mod.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+use tauri::api::path;
+
 pub struct AppConfig {}
 
 impl AppConfig {
@@ -8,4 +11,21 @@ impl AppConfig {
     pub fn get_locale() {}
 
     pub fn set_locale(&mut self, locale: &str) {}
+
+    // 配置文件路径:
+    pub fn config_dir() -> Option<PathBuf> {
+        path::config_dir()
+    }
+
+    pub fn document_dir() -> Option<PathBuf> {
+        path::document_dir()
+    }
+
+    pub fn cache_dir() -> Option<PathBuf> {
+        path::cache_dir()
+    }
+
+    pub fn data_dir() -> Option<PathBuf> {
+        path::data_dir()
+    }
 }
