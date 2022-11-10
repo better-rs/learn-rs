062663ab9d2f82ddfd09246e3def52e7a4620976
Henry.Huang <hhglory@outlook.com>
2022-11-10 10:22:29 +0800

    update

--- crates/rs-tauri-vue/src/components/Tab.vue
+++ crates/rs-tauri-vue/src/components/Tab.vue
@@ -1,51 +1,36 @@
-<script setup>
-import {NCard, NTabs, NTabPane} from 'naive-ui'
-import Home from './home/Home.vue'
-import Settings from './settings/Settings.vue'
-import Drawer from "./drawer/Drawer.vue";
-import Debug from "./debug/Debug.vue";
-import Demo from "./demo/Demo.vue"
-
-</script>
-
 <template>
 
-    <n-card title="" style="margin-bottom: 16px">
-        <n-tabs trigger="hover" default-value="demo" justify-content="space-evenly" type="line">
+    <n-card style="margin-bottom: 16px" title="">
 
-
-            <n-tab-pane name="demo" tab="Demo">
-                <Demo></Demo>
+        <!--         trigger="hover" -->
+        <n-tabs default-value="demo" justify-content="space-evenly" type="line">
+            <n-tab-pane v-for="t in tabs" :name="t.name" :tab="t.tab">
+                <!--  TODO X: 动态组件-->
+                <!--     https://cn.vuejs.org/api/sfc-script-setup.html#using-components -->
+                <component :is="t.widget"/>
             </n-tab-pane>
-
-
-            <n-tab-pane name="home" tab="首页">
-                <Home></Home>
-            </n-tab-pane>
-
-
-            <n-tab-pane name="trending" tab="行情">
-                行情页面
-            </n-tab-pane>
-
-            <n-tab-pane name="news" tab="资讯">
-                资讯页面
-            </n-tab-pane>
-
-            <n-tab-pane name="settings" tab="设置">
-                <Settings></Settings>
-            </n-tab-pane>
-
-            <n-tab-pane name="debug" tab="Debug">
-                <Debug></Debug>
-            </n-tab-pane>
-
-
-            <n-tab-pane name="drawer" tab="我的">
-                <Drawer></Drawer>
-            </n-tab-pane>
-
-
         </n-tabs>
+
     </n-card>
 </template>
+
+<script lang="ts" setup>
+import {NCard, NTabPane, NTabs} from 'naive-ui'
+import Home from './home/Home.vue'
+import Demo from "./demo/Demo.vue"
+import Debug from "./debug/Debug.vue"
+import Settings from "./settings/Settings.vue"
+import Drawer from "./drawer/Drawer.vue"
+
+
+const tabs = ref([
+    {"tab": "Demo", "name": "demo", "widget": Demo,},
+    {"tab": "Home", "name": "home", "widget": Home},
+    {"tab": "Debug", "name": "debug", "widget": Debug},
+    {"tab": "行情", "name": "trending", "widget": Settings},
+    {"tab": "资讯", "name": "news", "widget": Settings},
+    {"tab": "设置", "name": "settings", "widget": Settings},
+    {"tab": "我的", "name": "drawer", "widget": Drawer},
+]);
+
+</script>
