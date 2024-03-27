acdf24ee1692fb92fc01bddbd571eba0bf5a2504
Henry.Huang <hhglory@outlook.com>
2024-03-28 05:33:48 +0800

    update

---
+++ crates/rs-os/readme.md
@@ -0,0 +1,12 @@
+# rs-os
+
+- OS 操作系统 API 相关的库 & 使用示例.
+
+## rs-cross:
+
+- 跨平台相关的库
+
+### libs:
+
+- [try-notify](./rs-cross/try-notify): 系统通知(macOS/Windows/Linux)
+    - 基于: https://github.com/hoodie/notify-rust
--- crates/rs-os/rs-cross/try-notify/src/main.rs
+++ crates/rs-os/rs-cross/try-notify/src/main.rs
@@ -1,3 +1,23 @@
+#[cfg(target_os = "macos")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use notify_rust::Notification;
+
+    Notification::new()
+        .summary("Safari Crashed")
+        .body("Just kidding, this is just the notify_rust example.")
+        .appname("Toastify")
+        .icon("Toastify")
+        .show()?;
+
+    Ok(())
+}
+
+#[cfg(all(unix, not(target_os = "macos")))]
 fn main() {
-    println!("Hello, world!");
+    println!("this is a mac only feature")
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
+    println!("this is a mac only feature")
 }
