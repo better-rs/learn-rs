a679e677538456c5ef255f88c14b5ea55274ac56
Henry.Huang <hhglory@outlook.com>
2024-03-28 05:49:31 +0800

    update

---
+++ crates/rs-os/rs-cross/try-notify/src/run4.rs
@@ -0,0 +1,20 @@
+#![allow(unused_must_use)]
+
+use notify_rust::Notification;
+
+#[cfg(target_os = "macos")]
+static SOUND: &str = "Submarine";
+
+#[cfg(all(unix, not(target_os = "macos")))]
+static SOUND: &str = "message-new-instant";
+
+#[cfg(target_os = "windows")]
+static SOUND: &str = "Mail";
+
+fn main() {
+    Notification::new()
+        .summary("notification with sound")
+        .sound_name(SOUND)
+        .show()
+        .unwrap();
+}
